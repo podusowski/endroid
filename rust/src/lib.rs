44e55bf62cc186f41fbb01260e54341394a0ee0c
Piotr Podusowski <podusowski@gmail.com>
2024-12-27 16:54:18 +0100

    misc

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -44,17 +44,14 @@ impl Default for MyApp {
 impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("My egui Application");
-            ui.horizontal(|ui| {
-                let name_label = ui.label("Your name: ");
-                ui.text_edit_singleline(&mut self.name)
-                    .labelled_by(name_label.id);
-            });
-            ui.add(egui::Slider::new(&mut self.age, 0..=120).text("age"));
-            if ui.button("Increment").clicked() {
+            ui.heading("Egui on Android");
+
+            ui.label("Clipboard");
+            ui.text_edit_singleline(&mut self.name);
+
+            if ui.button("get").clicked() {
                 self.age += 1;
             }
-            ui.label(format!("Hello '{}', age {}", self.name, self.age));
         });
     }
 }
